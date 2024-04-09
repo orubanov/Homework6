8a23e5e9d3fc68882ad72b31474eea34a5e9b37e
Oleg Rubanov <165693738+orubanov@users.noreply.github.com>
2024-04-10 00:15:47 +0300

    Add files via upload

---
+++ hw6.py
@@ -0,0 +1,14 @@
+immutaple_var = 58, 'Hello', True, 0.75
+print(immutaple_var)
+immutaple_var [0] = 47
+immutaple_var [2] = 34 #кортеж относиться к неизменяемым типам данных
+mutable_list = [17, 'Bye', False, 0.25, 5, 'apple']
+print(mutable_list)
+mutable_list[0] = 4
+mutable_list[1] = 8
+mutable_list[2] = 15
+mutable_list[3] = 16
+mutable_list[4] = 23
+mutable_list[5] = 42
+print(mutable_list) #в списке можно изменять любой элемент
+
